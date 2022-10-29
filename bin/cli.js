ab0379cb9153f2c2a4119d96221fc7e7a184fe7a
Hu Guo <huhu8@ad.unc.edu>
2022-10-29 09:37:37 -0400

    test

--- bin/cli.js
+++ bin/cli.js
@@ -1,2 +1,32 @@
 #!/usr/bin/env node
-import { roll } from "/lib/roll.js"
+
+import { roll } from "../lib/roll.js"
+
+import minimist from 'minimist'
+
+const args = minimist(process.argv.slice(2))
+
+const side_in = args.side
+const dice_in = args.dice
+const rolls_in = args.rolls
+
+var side = 6
+var dice = 2
+var rolls = 1
+var result = []
+
+if (side_in) {
+    side = side_in
+}
+
+if (dice_in) {
+    dice = dice_in
+}
+
+if (rolls_in) {
+    rolls = rolls_in
+}
+
+const message_out = roll(side, dice, rolls)
+
+console.log(JSON.stringify(message_out))
